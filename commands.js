fd73fd797a3d3d7839f0911ac1ba58e9c84b21db
mashirochan <kylebarsh@gmail.com>
2015-05-31 16:14:06 -0400

    Create commands.js

---
+++ commands.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var sys = require('sys');
+
+if (config.serverid === 'showdown') {
+	var https = require('https');
+	var csv = require('csv-parse');
+}
+
+exports.commands = {
+
+};
